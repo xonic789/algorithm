c282641bd0292e2fbfa4da9d8cd382ce5a83ae99
SeungWooL <xonic789@naver.com>
2021-03-11 22:16:39 +0900

    쉬어가는 날..

---
+++ src/Programmers/곱합단계구하기.java
@@ -0,0 +1,31 @@
+package Programmers;
+
+public class 곱합단계구하기 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
+class Solution{
+
+    public int solution(int n){
+        int answer = 0;
+        int temp = 0;
+
+        while(temp==n){
+
+            for(int i=0;i<n;i++){
+                temp++;
+            }
+
+            
+
+        }
+
+        return answer;
+    }
+
+}
---
+++ src/Programmers/완주하지못한선수.java
@@ -0,0 +1,35 @@
+package Programmers;
+
+import java.util.*;
+
+public class 완주하지못한선수 {
+
+    public static void main(String[] args){
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+
+        Solution solution = new Solution();
+        solution.solution(participant,completion);
+    }
+    static class Solution {
+        public String solution(String[] participant, String[] completion) {
+            String answer = "";
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+            int i=0;
+            while(i < completion.length){
+                if(!participant[i].equals(completion[i])){
+                    return participant[i];
+                }else {
+                    i++;
+                }
+            }
+            if(answer.equals("")){
+               return participant[participant.length-1];
+            }
+
+
+            return answer;
+        }
+    }
+}
